9f78bde90b6decf994858bec0bc6da799076baac
tuxtrack <tuxtrack@hacknroll.com>
2021-01-20 16:42:16 -0300

    .

--- Program.cs
+++ Program.cs
@@ -27,7 +27,6 @@ namespace InspectAssembly
 
                     AssemblyName assemblyName = AssemblyName.GetAssemblyName(file);
 
-                    Console.WriteLine("ASSEMBLY: " + file.ToUpper() + '\n');
                     CheckMethods(file);
                 
                 }
@@ -38,6 +37,25 @@ namespace InspectAssembly
                 Console.WriteLine(excpt.Message);
             }
             
+             try
+            {
+
+                foreach (string file in Directory.EnumerateFiles(args[0], "*.dll", SearchOption.AllDirectories))
+                {
+
+                    // Make sure that the target is actually an assembly before we get started
+
+                    AssemblyName assemblyName = AssemblyName.GetAssemblyName(file);
+
+                    CheckMethods(file);
+                
+                }
+            }
+
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
              
         }
      
@@ -63,41 +81,51 @@ namespace InspectAssembly
                 foreach (var instruction in method.m.Body.Instructions)
                 {
                     //Console.WriteLine($"{instruction.OpCode} \"{instruction.Operand}\""); //DEBUG
-
+                    
                     // Deserialization checks
                     if (instruction.OpCode.ToString() == "callvirt")
                     {
                         switch (instruction.Operand.ToString())
                         {
                             case string x when x.Contains("System.Runtime.Serialization.Formatters.Binary.BinaryFormatter::Deserialize"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] BinaryFormatter::Deserialize() is called in {0}.{1}", method.t.Name, method.m.Name);
                                 Console.WriteLine("\tTypeFilterLevel: {0}", filterLevel + '\n');
                                 break;
                             case string x when x.Contains("System.Runtime.Serialization.Json.DataContractJsonSerializer::ReadObject"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] DataContractJsonSerializer::ReadObject() is called in {0}.{1}", method.t.Name, method.m.Name + '\n');
                                 break;
                             case string x when x.Contains("System.Runtime.Serialization.Xml.DataContractSerializer::ReadObject"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] DataContractSerializer::ReadObject() is called in {0}.{1}", method.t.Name, method.m.Name + '\n');
                                 break;
                             case string x when x.Contains("System.Web.Script.Serialization.JavaScriptSerializer::Deserialize"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] JavaScriptSerializer::Deserialize() is called in {0}.{1}", method.t.Name, method.m.Name + '\n');
                                 break;
                             case string x when x.Contains("System.Web.UI.LosFormatter::Deserialize"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] LosFormatter::Deserialize() is called in {0}.{1}", method.t.Name, method.m.Name + '\n');
                                 break;
                             case string x when x.Contains("System.Runtime.Serialization.NetDataContractSerializer::ReadObject"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] NetDataContractSerializer::ReadObject() is called in {0}.{1}", method.t.Name, method.m.Name + '\n');
                                 break;
                             case string x when x.Contains("System.Runtime.Serialization.NetDataContractSerializer::Deserialize"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] NetDataContractSerializer::Deserialize() is called in {0}.{1}", method.t.Name, method.m.Name + '\n');
                                 break;
                             case string x when x.Contains("System.Web.UI.ObjectStateFormatter::Deserialize"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] ObjectStateFormatter::Deserialize() is called in {0}.{1}", method.t.Name, method.m.Name + '\n');
                                 break;
                             case string x when x.Contains("System.Runtime.Serialization.Formatters.Soap.SoapFormatter::Deserialize"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] SoapFormatter::Deserialize() is called in {0}.{1}", method.t.Name, method.m.Name + '\n');
                                 break;
                             case string x when x.Contains("System.Xml.Serialization.XmlSerializer::Deserialize"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] XMLSerializer::Deserialize() is called in {0}.{1}", method.t.Name, method.m.Name + '\n'); 
                                 break;
 
@@ -139,9 +167,11 @@ namespace InspectAssembly
                         switch (instruction.Operand.ToString())
                         {
                             case string x when x.Contains("System.ServiceModel.ServiceHost::AddServiceEndpoint"):
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] Assembly appears to be a WCF server" + '\n');
                                 break;
                             case string x when x.Contains("System.ServiceModel.ChannelFactory") && x.Contains("CreateChannel"): // System.ServiceModel.ChannelFactory`1<ClassName.ClassName>::CreateChannel()
+                                Console.WriteLine(assemblyName.ToUpper());
                                 Console.WriteLine("[+] Assembly appears to be a WCF client" + '\n');
                                 break;
                         }
@@ -149,6 +179,7 @@ namespace InspectAssembly
                 }
             }
         }
-
     }
+
+  
 }
